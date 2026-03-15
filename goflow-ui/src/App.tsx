a5d146721ed9d88c2b678a6de56ae4732cc1197a
Vivek Hipparkar <vivekhipparkar@gmail.com>
2026-03-15 15:52:43 +0530

    frontend added

---
+++ goflow-ui/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        GoFlow UI
+      </h1>
+    </div>
+  )
+}
